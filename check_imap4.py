bb552bcb1265f0c109aa92281072c8d852ffd868
David Lukas Müller <dlmueller@Davids-MacBook-Pro.local>
2015-07-16 11:35:04 +0200

    added modified version from Bernhard Schmidt (2012)

--- check_imap4.py
+++ check_imap4.py
@@ -4,6 +4,7 @@
 # -*- Mode: Python -*-
 #
 # Copyright (C) 2005 Bertera Pietro <pietro@bertera.it>
+# Copyright (C) 2012 Bernhard Schmidt <berni@birkenwald.de>
 
 # This file may be distributed and/or modified under the terms of
 # the GNU General Public License version 2 as published by
@@ -11,8 +12,10 @@
 # This file is distributed without any warranty; without even the implied
 # warranty of merchantability or fitness for a particular purpose.
 # See "LICENSE.GPL" in the source distribution for more information.
+# 
+# Version 20121106
 
-import sys, os, imaplib, getopt
+import sys, os, imaplib, getopt, time
 
 def usage():
     print "-u <user>"
@@ -21,8 +24,8 @@ def usage():
     print "-H <host>"
 
 def main():
-	try:
-	    opts, args = getopt.getopt(sys.argv[1:], "u:p:sH:")	
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "u:p:sH:")
     except getopt.GetoptError:
         usage()
         return 3
@@ -41,22 +44,37 @@ def main():
     if user == None or password == None or host == None:
         usage()
         return 1
+
+    timepreconnect = time.time()
     
-    if use_ssl:
-        M = imaplib.IMAP4_SSL(host=host)
-    else:
-    	M = imaplib.IMAP4(host)
+    try:
+        if use_ssl:
+            M = imaplib.IMAP4_SSL(host=host)
+        else:
+            M = imaplib.IMAP4(host)
+    except Exception, e:
+        print "CRITICAL: Could not connect to %s: %s" % (host, e)
+        return 2
+    
+    timeprelogin = time.time()
     
     try:	
         M.login(user, password)
-	except Exception, e:
+    except Exception, e:
         print "CRITICAL: IMAP Login not Successful: %s" % e
-        sys.exit(2)
+        return 2
     
+    timepostlogin = time.time()
+
+    connectdelay = (timeprelogin-timepreconnect)*1000
+    logindelay = (timepostlogin-timeprelogin)*1000
+
     M.logout()
-    print "OK IMAP Login Successful"
+    print "OK IMAP Login Successful (Connect: %.2fms, Login: %.2fms) | connect=%.2fms login=%.2fms" % ( 
+        connectdelay, logindelay,
+        connectdelay, logindelay )
     return 0
 
 if __name__ == "__main__":
-        sys.exit(main())
-    
+    sys.exit(main())
+
