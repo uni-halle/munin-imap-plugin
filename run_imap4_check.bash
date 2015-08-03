b87794ec0c9812b70cd6d3d28db8b182100d650c
David Lukas Müller <dlmueller@Davids-MacBook-Pro.local>
2015-08-03 11:06:06 +0200

    added bash script with login parameters from environment

---
+++ run_imap4_check.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+# This script expects the following environment variables to be set:
+#
+#     - TEST_MAIL_INTERNAL_IMAP_HOST
+#     - TEST_MAIL_INTERNAL_USERNAME
+#     - TEST_MAIL_INTERNAL_PASSWORD
+#
+# Usage
+#
+#    ./run_imap4.check.bash config
+#
+#    ./run_imap4.check.bash
+#
+# VERBOSITY = 0 or 1
+_V=0
+
+function log () {
+    if [[ $_V -eq 1 ]]; then
+        echo "$@"
+    fi
+}
+
+log "DEBUG: argument 0 = $0"
+log "DEBUG: argument 1 = $1"
+
+log "DEBUG: username = $TEST_MAIL_INTERNAL_USERNAME"
+log "DEBUG: hostname = $TEST_MAIL_INTERNAL_IMAP_HOST"
+
+if [[ "$1" = "config" ]]; then
+    python ./check_imap4.py config
+else
+    python ./check_imap4.py -u "$TEST_MAIL_INTERNAL_USERNAME" -p "$TEST_MAIL_INTERNAL_PASSWORD" -H "$TEST_MAIL_INTERNAL_IMAP_HOST" -s
+fi
