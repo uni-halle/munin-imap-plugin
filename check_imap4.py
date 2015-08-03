355a247463a7c7fa32d183b9034ac9c0e8a6b0e8
David Lukas Müller <dlmueller@users.noreply.github.com>
2015-08-03 19:04:43 +0200

    feature added (disabled): read latest mail from each folder/mailbox

--- check_imap4.py
+++ check_imap4.py
@@ -27,6 +27,7 @@ import time
 #---
 #--- Python (Mail)
 import imaplib
+import email
 
 #---
 #--- Plugin Stuff
@@ -85,128 +86,46 @@ def iterMailboxNames(conn) :
         mbNameWithQuotes = mbParts[-1].strip()
         firstPart = mbParts[0].strip()[1:-1]
         markers = list(m.lower().strip() for m in firstPart.split(' '))
-        mbName = mbNameWithQuotes[1:-1]
+        if mbNameWithQuotes.startswith('"') or mbNameWithQuotes.startswith("'") :
+            mbName = mbNameWithQuotes[1:-1]
+        else :
+            mbName = mbNameWithQuotes
         yield mbName, markers
-        continue
 
-def printCapabilities(conn, capabilities) :
+
+
+def iterMailboxContent_sequentialID(conn, mbName) :
     """
-    What capabilities does the IMAP server support?
-    On http://www.iana.org/assignments/imap-capabilities/imap-capabilities.xhtml
-    the following capabilities are listed::
-
-        ACL                     [RFC4314]
-        ANNOTATE-EXPERIMENT-1   [RFC5257]
-        AUTH=                   [RFC3501]
-        BINARY                  [RFC3516]
-        CATENATE                [RFC4469]
-        CHILDREN                [RFC3348]
-        COMPRESS=DEFLATE        [RFC4978]
-        CONDSTORE               [RFC7162]
-        CONTEXT=SEARCH          [RFC5267]
-        CONTEXT=SORT            [RFC5267]
-        CONVERT                 [RFC5259]
-        CREATE-SPECIAL-USE      [RFC6154]
-        ENABLE                  [RFC5161]
-        ESEARCH                 [RFC4731]
-        ESORT                   [RFC5267]
-        FILTERS                 [RFC5466]
-        I18NLEVEL=1             [RFC5255]
-        I18NLEVEL=2             [RFC5255]
-        ID                      [RFC2971]
-        IDLE                    [RFC2177]
-        IMAPSIEVE=              [RFC6785]
-        LANGUAGE                [RFC5255]
-        LIST-EXTENDED           [RFC5258]
-        LIST-STATUS             [RFC5819]
-        LITERAL+                [RFC2088]
-        LOGIN-REFERRALS         [RFC2221]
-        LOGINDISABLED           [RFC2595][RFC3501]
-        MAILBOX-REFERRALS       [RFC2193]
-        METADATA                [RFC5464]
-        METADATA-SERVER         [RFC5464]
-        MOVE                    [RFC6851]
-        MULTIAPPEND             [RFC3502]
-        MULTISEARCH             [RFC7377]
-        NAMESPACE               [RFC2342]
-        NOTIFY                  [RFC5465]
-        QRESYNC                 [RFC7162]
-        QUOTA                   [RFC2087]
-        RIGHTS=                 [RFC4314]
-        SASL-IR                 [RFC4959]
-        SEARCH=FUZZY            [RFC6203]
-        SEARCHRES               [RFC5182]
-        SORT                    [RFC5256]
-        SORT=DISPLAY            [RFC5957]
-        SPECIAL-USE             [RFC6154]
-        STARTTLS                [RFC2595][RFC3501]
-        THREAD                  [RFC5256]
-        UIDPLUS                 [RFC4315]
-        UNSELECT                [RFC3691]
-        URLFETCH=BINARY         [RFC5524]
-        URL-PARTIAL             [RFC5550]
-        URLAUTH                 [RFC4467]
-        UTF8=ACCEPT             [RFC6855]
-        UTF8=ALL     (OBSOLETE) [RFC5738][RFC6855]
-        UTF8=APPEND  (OBSOLETE) [RFC5738][RFC6855]
-        UTF8=ONLY               [RFC6855]
-        UTF8=USER    (OBSOLETE) [RFC5738][RFC6855]
-        WITHIN                  [RFC5032]
-
-    Other important RFCs are::
-
-        RFC2060  INTERNET MESSAGE ACCESS PROTOCOL - VERSION 4rev1
-            obsoletes RFC1730
-
-        RFC3501  INTERNET MESSAGE ACCESS PROTOCOL - VERSION 4rev1
-            obsoletes RFC2060
-            updated by RFC4466 Collected Extensions to IMAP4 ABNF
-            updated by RFC4469 Internet Message Access Protocol (IMAP) CATENATE Extension
-            updated by RFC4551 IMAP Extension for Conditional STORE Operation
-                               or Quick Flag Changes Resynchronization
-            updated by RFC5032 WITHIN Search Extension to the IMAP Protocol
-            updated by RFC5182 IMAP Extension for Referencing the Last SEARCH Result
-            updated by RFC5738 IMAP Support for UTF-8
-            updated by RFC6186 Use of SRV Records for Locating Email Submission/Access Services
-            updated by RFC6858 Simplified POP and IMAP Downgrading for Internationalized Email
-
-        RFC2222 Simple Authentication and Security Layer (SASL)
-
-
-
-    @param capabilites: contains the CAPABILITIES of the IMAP server
-    @type  capabilites: tuple of str
+    @precondition: Mailbox must be SELECTED
+    @return: generator[(id, rawMail)]
     """
+    # Sequential IDs (SID)
+    sidResult, sidData = conn.search(None, "ALL") # returns sequential id
+    sid_string = sidData[0]
+    sid_list = sid_string.split() # separated by SPACE
 
-    #print "  namespace: %r" % M.namespace()
+    for sid in reversed(sid_list) :
+        mailResult, mailData = conn.fetch(sid, "(RFC822)") # feth the body
+        sid_raw_email = mailData[0][1]
+        yield (sid, raw_email)
+        break
 
-    # Some methods from the Python library 'imaplib' will only work
-    # if the server does support the specific capabilities.
-    capabilitiesOfInterest = set(capabilities)
-    capabilitiesOfInterest.add('ACL') # e.g. Cyrus Server
-    capabilitiesOfInterest.add('ANNOTATIONS') # non-standard, but supported by Cyrus Server
-    capabilitiesOfInterest.add('CHILDREN') # /Noinferiors
-    capabilitiesOfInterest.add('QUOTA')
-    capabilitiesOfInterest.add('AUTH=CRAM-MD5')
-    capabilitiesOfInterest.add('NAMESPACE')
 
-    # Novel Groupwiese
-    # https://www.novell.com/documentation/groupwise_sdk/gwsdk_gwimap/data/al7te9j.html
-    capabilitiesOfInterest.add('XGWEXTENSIONS')
 
-    print
-    print "  %-20s | supported " % ("Capabilities",)
-    print "  %s-+-----------" % ("-"*20,)
+def iterMailboxContent_uniqueID(conn, mbName) :
+    """
+    @precondition: Mailbox must be SELECTED
+    @return: generator[(id, rawMail)]
+    """
+    # Unique IDs (UID)
+    uidResult, uidData = conn.uid('search', None, "ALL")
+    uid_list = uidData[0].split()
+    for uid in reversed(uid_list) :
+        mailResult, mailData = conn.uid('fetch', uid, '(RFC822)')
+        raw_email = mailData[0][1]
+        yield (uid, raw_email)
+        break
 
-    for capName in sorted(capabilitiesOfInterest) :
-        supported = capName in capabilities
-        print "  %-20s | %s" % (capName, "%s" % ('SUPPORTED' if supported else 'NO'))
-
-#    for xcapName in sorted(capabilitiesOfInterest) :
-#        if xcapName.startswith('X') :
-#            print
-#            print xcapName
-#            print conn.xatom(xcapName)
 
 
 def printMailboxesWithItemCount(conn) :
@@ -225,12 +144,54 @@ def printMailboxesWithItemCount(conn) :
         markerString = "MARKED" if isMarked else "  NO  "
         attributeString = ", ".join(sorted(attributeSet))
         msgCount = msgCountList[0]
-        #acl = M.myrights(mailbox) # works only with ACL
-        #quotaRoots = M.getquotaroot(mailbox) # works only with QUOTA
-        (okCheck, checkResultList) = conn.check()
-        checkResult = checkResultList[0]
+
+        if 0 :
+            acl = M.myrights(mailbox) # works only with ACL
+
+        if 0 :
+            quotaRoots = M.getquotaroot(mailbox) # works only with QUOTA
+
+        if 0 :
+            (okCheck, checkResultList) = conn.check()
+            checkResult = checkResultList[0]
+
         print "  %(mailbox)-20s | %(msgCount)5s  | %(markerString)6s | %(attributeString)s " % locals()
 
+def printMailboxesWithLatestMail(conn) :
+    """
+    Prints all mailboxes with the lates message on top
+    """
+    for mailbox, markers in iterMailboxNames(conn) :
+        (okSelect, msgCountList) = conn.select(mailbox, readonly = True)
+        print "%(mailbox)s" % locals()
+        #iterMailboxContent = iterMailboxContent_sequentialID
+        iterMailboxContent = iterMailboxContent_uniqueID
+
+        for id, rawMail in iterMailboxContent(conn, mailbox) :
+            email_message = email.message_from_string(rawMail)
+            print "UID = %(id)s" % locals()
+            emailTo =email_message['To']
+            emailFrom = email.utils.parseaddr(email_message['From'])
+            #print "To = %s" % (emailTo,)
+            #print "From = %s" % (emailFrom,)
+            for emailHeader in email_message.items() :
+                print "    %s" % (emailHeader,)
+
+            if 0 :
+                # note that if you want to get text content (body) and the email contains
+                # multiple payloads (plaintext/ html), you must parse each message separately.
+                # use something like the following: (taken from a stackoverflow post)
+                def get_first_text_block(self, email_message_instance):
+                    maintype = email_message_instance.get_content_maintype()
+                    if maintype == 'multipart':
+                        for part in email_message_instance.get_payload():
+                            if part.get_content_maintype() == 'text':
+                                return part.get_payload()
+                    elif maintype == 'text':
+                        return email_message_instance.get_payload()
+
+            print
+        print
 
 def HandleInvalidArguments(cli) :
     """
@@ -286,6 +247,12 @@ def HandleSuccessfulLogin(cli, conn, connectDelay, loginDelay) :
 
     HandleMeasureCommand(cli, loginDelay)
     #HandleMeasureCommand(cli, connectDelayd)
+
+    if 0 :
+        printMailboxesWithItemCount(conn)
+
+    if 0 :
+        printMailboxesWithLatestMail(conn)
 
     conn.logout()
     return cli.MapNagiosReturnCode(nagios_stuff.NAGIOS_RC_OK)
